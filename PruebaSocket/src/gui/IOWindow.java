4df0f5c389b6f1e7991c902c6a367af62a3e1a4b
Bitdeparidad <francisjavi2@gmail.com>
2020-10-21 22:57:48 +0200

    Rama? no se en que rama se subirá closes #1

--- PruebaSocket/src/gui/IOWindow.java
+++ PruebaSocket/src/gui/IOWindow.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor. 
  */
+//a ver a que rama se sube esto
 package gui;
 
 import java.io.IOException;
