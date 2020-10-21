79578ada68446fa16c936b0fc606562501f83dac
Bitdeparidad <francisjavi2@gmail.com>
2020-10-21 23:21:52 +0200

    Revert "Añadidos comentarios al código."
    This reverts commit 9903e01833631a3d905413e624ae75958a667b21.

--- PruebaSocket/src/server/EchoServer.java
+++ PruebaSocket/src/server/EchoServer.java
@@ -22,30 +22,22 @@ public class EchoServer {
         int port = 80;
         Socket client;
         String str = "ECHO: ";
-        //abrir canal para conexiones entrantes
         serverSocket = new ServerSocket(port);
         System.out.println("Esperando conexion en: "+ InetAddress.getLocalHost()+"...");
-        //aceptar conexion entrante
         client = serverSocket.accept();
         System.out.println("Conectado.");
         
         String input = "";
-        //abrir entrada de datos para la conexion
         ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
-        //abrir salida de datos para la conexion
         ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
         while(!"adios".equals(input)){
-            //recibir mensaje
             input = (String) ois.readObject();
             System.out.println(input);
-            //mandar respuesta
             oos.writeObject(str+input);
         }
-        //cerrar canal de entada
+        
         ois.close();
-        //cerrar canal de salida
         oos.close();
-        //cerrar comunicacion
         client.close();
     }
 }
